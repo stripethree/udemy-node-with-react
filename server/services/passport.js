bf4f582cb46f6e011a5b72581bfd850c57667a22
Jeff Israel <jeff.israel@gmail.com>
2019-12-26 14:46:28 -0600

    Working on passport callback fulfillment

--- server/services/passport.js
+++ server/services/passport.js
@@ -1,7 +1,10 @@
 const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const mongoose = require("mongoose");
 
 const keys = require("../config/keys");
+
+const User = mongoose.model("users");
 
 passport.use(
   new GoogleStrategy(
@@ -11,9 +14,16 @@ passport.use(
       callbackURL: "/auth/google/callback"
     },
     (accessToken, refreshToken, profile, done) => {
-      console.log("accessToken: ", accessToken);
-      console.log("refreshToken: ", refreshToken);
-      console.log("profile: ", profile);
+      User.findOne({ googleId: profile.id }).then(existingUser => {
+        if (existingUser) {
+          // we already have a record with the given googleId
+          done(null, existingUser);
+        } else {
+          new User({ googleId: profile.id })
+            .save()
+            .then(newUser => done(null, newUser));
+        }
+      });
     }
   )
 );
