b658fa01da65997eb9eacc17d33cde653ac44431
Jeff Israel <jeff.israel@gmail.com>
2019-12-28 15:58:52 -0600

    Hooking up action creators

--- server/client/src/components/App.js
+++ server/client/src/components/App.js
@@ -1,5 +1,8 @@
 import React, { Component } from "react";
 import { BrowserRouter, Route } from "react-router-dom";
+import { connect } from "react-redux";
+
+import * as actions from "../actions";
 
 import Header from "./Header";
 const Dashboard = () => <h2>Dashboard</h2>;
@@ -7,7 +10,9 @@ const SurveyNew = () => <h2>SurveyNew</h2>;
 const Landing = () => <h2>Landing</h2>;
 
 class App extends Component {
-  componentDidMount() {}
+  componentDidMount() {
+    this.props.fetchUser();
+  }
 
   render() {
     return (
@@ -25,4 +30,7 @@ class App extends Component {
   }
 }
 
-export default App;
+export default connect(
+  null,
+  actions
+)(App);
