e6521919d6e836d33f30b1e1f333c4448bb3e00e
Jeff Israel <jeff.israel@gmail.com>
2019-12-28 16:53:48 -0600

    Finish login - logout flow

--- server/client/src/components/Header.js
+++ server/client/src/components/Header.js
@@ -15,7 +15,7 @@ class Header extends Component {
       default:
         return (
           <li>
-            <a href="/auth/google">Logout</a>
+            <a href="/api/logout">Logout</a>
           </li>
         );
     }
