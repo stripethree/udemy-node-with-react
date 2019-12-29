da8bf63775a3820458e492b97a145d4ff50b2aa2
Jeff Israel <jeff.israel@gmail.com>
2019-12-29 17:27:05 -0600

    More fields on the survey model

--- server/models/Survey.js
+++ server/models/Survey.js
@@ -5,11 +5,14 @@ const RecipientSchema = require("./recipient");
 
 const surveySchema = new Schema({
   body: String,
+  dateSent: Date,
+  lastResponded: Date,
   recipients: [RecipientSchema],
   subject: String,
   title: String,
   yes: { type: Number, default: 0 },
-  no: { type: Number, default: 0 }
+  no: { type: Number, default: 0 },
+  _user: { type: Schema.Types.ObjectId, ref: "User" }
 });
 
 mongoose.model("surveys", surveySchema);
