77aace2206d1c1c3a64aa6ef71bb9b6af720317d
Jeff Israel <stripethree@users.noreply.github.com>
2019-12-30 13:49:11 -0600

    Mongoose for Survey Creation (#10)
    Adding models via mongoose for surveys and wiring up survey creation including transactional email with SendGrid.

--- server/index.js
+++ server/index.js
@@ -6,6 +6,7 @@ const passport = require("passport");
 
 const keys = require("./config/keys");
 require("./models/User");
+require("./models/Survey");
 require("./services/passport");
 
 mongoose.connect(keys.mongoURI);
@@ -23,6 +24,7 @@ app.use(passport.session());
 
 require("./routes/authRoutes")(app);
 require("./routes/billingRoutes")(app);
+require("./routes/surveyRoutes")(app);
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
---
+++ server/models/Survey.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const RecipientSchema = require("./recipient");
+
+const surveySchema = new Schema({
+  body: String,
+  dateSent: Date,
+  lastResponded: Date,
+  recipients: [RecipientSchema],
+  subject: String,
+  title: String,
+  yes: { type: Number, default: 0 },
+  no: { type: Number, default: 0 },
+  _user: { type: Schema.Types.ObjectId, ref: "User" }
+});
+
+mongoose.model("surveys", surveySchema);
