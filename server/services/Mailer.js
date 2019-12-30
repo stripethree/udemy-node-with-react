6ac48b49172700f3a0271fad11aede65e2b7d6b0
Jeff Israel <jeff.israel@gmail.com>
2019-12-30 12:03:36 -0600

    More on the Mailer service

--- server/services/Mailer.js
+++ server/services/Mailer.js
@@ -2,6 +2,38 @@ const sendgrid = require("sendgrid");
 const helper = sendgrid.mail;
 const keys = require("../config/keys");
 
-class Mailer extends helper.Mail {}
+class Mailer extends helper.Mail {
+  constructor({ subject, recipients }, content) {
+    super();
+    this.from_email = new helper.Email("no-reply@jvolpe.com");
+    this.subject = subject;
+    this.body = new helper.Content("text/html", content);
+    this.recipients = this.formatAddresses(recipients);
+
+    this.addContent(this.body);
+    this.addClickTracking();
+    this.addRecipients();
+  }
+
+  addClickTracking() {
+    const trackingSettings = new helper.TrackingSettings();
+    const clickTracking = new helper.ClickTracking(true, true);
+
+    trackingSettings.setClickTracking(clickTracking);
+    this.addTrackingSettings(trackingSettings);
+  }
+
+  addRecipients() {
+    const personalize = new helper.Personalization();
+    this.recipients.forEach(recipient => {
+      personalize.addTo(recipient);
+    });
+    this.addPersonalization(personalize);
+  }
+
+  formatAddressres(recipients) {
+    return recipients.map(({ email }) => new helper.Email(email));
+  }
+}
 
 module.exports = Mailer;
