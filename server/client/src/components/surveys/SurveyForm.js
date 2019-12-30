8870d0106117aded46c61b9acf9023029f4fbc84
Jeff Israel <jeff.israel@gmail.com>
2019-12-30 16:19:15 -0600

    Field and button styling

--- server/client/src/components/surveys/SurveyForm.js
+++ server/client/src/components/surveys/SurveyForm.js
@@ -1,6 +1,7 @@
 import _ from "lodash";
 import React, { Component } from "react";
 import { reduxForm, Field } from "redux-form";
+import { Link } from "react-router-dom";
 
 import SurveyField from "./SurveyField";
 
@@ -43,7 +44,12 @@ class SurveyForm extends Component {
       <div>
         <form onSubmit={this.props.handleSubmit(values => console.log(values))}>
           {this.renderFields()}
-          <button type="submit">Submit</button>
+          <Link className="red btn-flat white-text" to="/surveys">
+            Cancel
+          </Link>
+          <button className="teal btn-flat right white-text" type="submit">
+            Next <i className="material-icons right">done</i>
+          </button>
         </form>
       </div>
     );
