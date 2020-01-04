a13dc919d4a5f35d837f467fd02e7ab57dfd8b25
Jeff Israel <jeff.israel@gmail.com>
2020-01-04 14:24:10 -0600

    Register SurveyDraft model

--- server/index.js
+++ server/index.js
@@ -7,6 +7,7 @@ const passport = require("passport");
 const keys = require("./config/keys");
 require("./models/User");
 require("./models/Survey");
+require("./models/SurveyDraft");
 require("./services/passport");
 
 mongoose.Promise = global.Promise;
